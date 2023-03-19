8296166b1e6a24084f2c7e0272b0067ed4541d17
Egor <egor032324@mail.ru>
2023-03-19 15:45:52 +0300

    Projects

---
+++ French-English/main.py
@@ -0,0 +1,65 @@
+from tkinter import *
+import random
+import pandas
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("French-English/data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("French-English/data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+
+def next_card():
+    global current_card,flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title,text = "French",fill = "black")
+    canvas.itemconfig(card_word,text = current_card["French"],fill = "black")
+    canvas.itemconfig(card_background,image = card_front_img)
+    flip_timer = window.after(3000,func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title,text = "English",fill = "white")
+    canvas.itemconfig(card_word,text = current_card["English"],fill = "white")
+    canvas.itemconfig(card_background,image = card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("French-English/data/word_to_learn.csv",index = False)
+    next_card()
+
+window = Tk()
+window.title("Flashy")
+window.config(bg = BACKGROUND_COLOR, padx = 50, pady = 50)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800,height=526,bg = BACKGROUND_COLOR,highlightthickness=0)
+card_front_img = PhotoImage(file = "French-English/images/card_front.png")
+card_back_img = PhotoImage(file = "French-English/images/card_back.png")
+
+card_background = canvas.create_image(400,263,image = card_front_img)
+card_title = canvas.create_text(400, 150, text = "Title",font = ("Arial", 40, "italic"))
+card_word = canvas.create_text(400,263,text = "word",font = ("Arial",60,"bold"))
+canvas.grid(row = 0, column= 0 ,columnspan= 2)
+
+check_img = PhotoImage(file = "French-English/images/right.png")
+right_button = Button(image = check_img,bg = BACKGROUND_COLOR,highlightthickness=0,command=next_card)
+right_button.grid(row = 1,column=0)
+
+wrong_img = PhotoImage(file = "French-English/images/wrong.png")
+wrong_button = Button(image = wrong_img,bg = BACKGROUND_COLOR,highlightthickness=0,command=is_known)
+wrong_button.grid(row = 1,column=1)
+
+next_card
+
+window.mainloop()
